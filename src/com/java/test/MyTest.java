15e3dfac6268ee66b48c904cbb4a42e7c03bfc4e
zhangning <zhangning@baicaowei.cn>
2019-12-06 15:01:52 +0800

    修改代码

--- src/com/java/test/MyTest.java
+++ src/com/java/test/MyTest.java
@@ -7,5 +7,6 @@ package com.java.test;
 public class MyTest {
     public static void main(String[] args) {
         System.out.println("提交到githup上.......");
+        System.out.println("在分支20191206上修改代码........");
     }
 }
